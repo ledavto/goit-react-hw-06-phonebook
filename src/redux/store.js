f84944f6208b836df721806f17197d82a750a3c4
ledavto <ser_ega@ukr.net>
2023-12-07 15:50:03 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,36 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+import { createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
 
-const initialState = 
-{
+const initialState = {
   contacts: [],
-  filter: ""
+  filter: '',
 };
 
-const rootReducer = (state = initialState, action) => {  
-return state;
- };
+const reducer = (state = initialState, action) => {
+  if (action.type === 'filterStr') {
+    return { ...state, filter: action.payload };
+  }
+
+  if (action.type === 'addUser') {
+    return { ...state, contacts: [...state.contacts, action.payload] };
+  }
+  return state;
+};
 
 // Створюємо розширення стора, щоб додати інструменти розробника
-const enhancer = devToolsEnhancer();
-export const store = createStore (rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(reducer, enhancer);
+
+export const store = createStore(reducer, initialState);
+
+store.dispatch({ type: 'filterStr', payload: 'string' });
+
+console.log(store.getState());
+
+store.dispatch({ type: 'addUser', payload: 'Vasya' });
+
+console.log(store.getState());
+
+//store.getState();
 
 //export const store = createStore(rootReducer);
